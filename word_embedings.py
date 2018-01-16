219cba9a64aa75418d995d571bae0800ffa31619
Alonzo <alonzo.q@gmail.com>
2018-01-16 17:02:47 -0500

    Fix sentence size to 40 embeddings with dimension of 300

---
+++ word_embedings.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+import re
+from nltk.corpus import stopwords
+from gensim.models import Word2Vec
+
+TRAINING_SENTENCES = 10000
+TRAINING_FILE ='embeddings_training/training_for_embeddings_'+str(TRAINING_SENTENCES) 
+
+stop = stopwords.words('spanish')
+
+def clean_dataset_for_embedings(path,output):
+    i = open(path,'r')
+    o = open(output,'w')
+    for w in range(TRAINING_SENTENCES):
+        if w % 100 == 0:
+            print w*1.0/TRAINING_SENTENCES*100,'%'
+        s = i.readline()
+        if len(s) > 6 :
+            #print s
+            s = clean_str(s)
+            #print s
+            o.write(s)
+    i.close()
+    o.close()
+
+def load_word2vec(path,binary = False):
+    from gensim.models.keyedvectors import KeyedVectors
+    word_vectors = KeyedVectors.load_word2vec_format(path, binary=binary)
+    #print word_vectors['nada']
+    return word_vectors
+
+def clean_str(str_input):
+    str_accent =  ['año' ,'á','é','è','í','ó','ú','ñ','%','#','@','"','/','-','°','(',')','[',']','.',',',':',';']
+    str_replace = ['anio','a','e','e','i','o','u','n','' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ]
+    #str_input = str_input.encode("utf-8")
+    for s in range(len(str_accent)):
+        str_input = str_input.replace(str_accent[s],str_replace[s])
+    return re.sub(' +',' ',str_input).lower().rstrip()
+
+def train_word2vec(path):
+    i = open(path,'r')
+    data = []
+    for l in range(TRAINING_SENTENCES):
+        s = i.readline()
+        data.append(s.split(' '))
+    i.close()
+    model = Word2Vec(data, size=300, window=5, min_count=5, workers=4)
+    model.wv.save_word2vec_format('embeddings_models/model_word2vec_'+str(TRAINING_SENTENCES),binary = False)
+    return model
+
+def train_fasttext(path):
+    # https://pypi.python.org/pypi/fasttext
+    import fasttext
+    model = fasttext.skipgram(path, 'embeddings_models/model_fasttext_'+str(TRAINING_SENTENCES),dim=300)
+    
+def train_glove(path):
+    import itertools
+    from gensim.models.word2vec import Text8Corpus
+    from glove import Corpus, Glove
+    sentences = list(itertools.islice(Text8Corpus(path),None))
+    corpus = Corpus()
+    corpus.fit(sentences, window=10)
+    glove = Glove(no_components=300, learning_rate=0.05)
+    glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
+    glove.add_dictionary(corpus.dictionary)
+    glove.save('embeddings_models/model_glove_'+str(TRAINING_SENTENCES))
+    return glove
+
+def get_word2vec_encode(word):
+    if word in m_word2vec:
+        return m_word2vec[word]
+    return None
+
+def get_glove_encode(word):
+    if word in m_glove:
+        return m_glove[word]
+
+def get_fasttext_encode(word):
+    if word in m_fasttext:
+        return m_fasttext[word]
+
+def build_data_set_from_xml(xml_path):
+    SENTIMENT_POSITIVE = 'P'
+    SENTIMENT_NEGATIVE = 'N'
+    SENTIMENT_NONE = 'NONE'
+    SENTIMENT_NEUTRAL = 'NEU'
+
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    data = []
+    for tweet in root.findall('tweet'):
+        temp = []
+        content = tweet.find('content').text
+        content = content.encode("utf-8")
+        content = clean_str(content)
+        temp.append(content)
+        polarity = tweet.find('sentiment').find('polarity').find('value').text
+        if polarity == SENTIMENT_POSITIVE:
+            temp.append(1)
+        elif polarity == SENTIMENT_NEGATIVE:
+            temp.append(-1)
+        elif polarity == SENTIMENT_NEUTRAL:
+            temp.append(0)
+        else :
+            temp.append(2)
+        data.append(temp)
+    return data
+
+#%% Get batch for training
+# Call Example 
+# batch,sentiment = getBatch(data,1,10)
+def get_batch(data,start,end):
+    max_size = 40
+    batch = []
+    sentiment = []
+    for b in range(start,end):
+        dim_1 = []
+        dim_2 = []
+        dim_3 = []
+        content,s = data[b]
+        words = content.split(' ')
+        sentence = []
+        for w in words:
+            d_1 = get_word2vec_encode(w)
+            d_2 = get_glove_encode(w)
+            d_3 = get_fasttext_encode(w)
+            embed = []
+            if d_1 is not None and d_2 is not None and d_3 is not None:
+                embed.append(d_1)
+                embed.append(d_2)
+                embed.append(d_3)
+            sentence.append(embed)
+        if sentence:
+            if len(sentence) < max_size:
+                deff = max_size - len(sentence)
+                embed = []
+                embed.append([0.0]*300)
+                embed.append([0.0]*300)
+                embed.append([0.0]*300)
+                for i in range(deff):
+                    sentence.append(embed)
+                #print len(sentence)
+                #print max_size
+                #print content
+            batch.append(sentence)
+            sentiment.append(s)
+    return batch,sentiment
+
+def load_fast_text(path):
+    import fasttext
+    model = fasttext.load_model(path, encoding='utf-8')
+    return model
+
+def load_glove(path):
+    #python -m gensim.scripts.glove2word2vec -i model_glove_100 -o model_glove_100_modified
+    return load_word2vec(path,binary = True)
+
+def train_embeddings():
+    clean_dataset_for_embedings('/home/alonzo/Documentos/Projects/wikipedia_dataset.txt',TRAINING_FILE)
+    print 'Start fasttext'
+    train_fasttext(TRAINING_FILE)
+    print 'Start glove'
+    train_glove(TRAINING_FILE)
+    print 'Start word2vec'
+    train_word2vec(TRAINING_FILE)
+
+#train_embeddings()
+
+#m_fasttext = load_fast_text('embeddings_models/model_fasttext_'+str(TRAINING_SENTENCES)+'.vec')
+m_fasttext = load_word2vec('embeddings_models/model_fasttext_'+str(TRAINING_SENTENCES)+'.vec',binary = False)
+#m_glove = load_glove('embeddings_models/model_glove_'+str(TRAINING_SENTENCES)+'_modified')
+m_word2vec = load_word2vec('embeddings_models/model_word2vec_'+str(TRAINING_SENTENCES),binary = False)
+m_glove    = load_word2vec('embeddings_models/model_word2vec_'+str(TRAINING_SENTENCES),binary = False)
+
+data = build_data_set_from_xml('datasets/intertass-train-tagged.xml')
+#data = build_data_set_from_xml('datasets/intertass-test.xml')
+batch,sentiment = get_batch(data,0,10)
+#print batch, sentiment
